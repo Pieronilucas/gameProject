f5eca3313b74be3a47617a97d515c4878c85697a
Lucas Pieroni <lucaspieroni819918@outlook.com>
2025-02-24 15:56:18 -0300

    first gaming commit

---
+++ src/main/java/GameProject/Main.java
@@ -0,0 +1,13 @@
+package GameProject;
+
+import Jade.Window;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
+
+    }
+}
---
+++ src/main/java/Jade/Window.java
@@ -0,0 +1,95 @@
+package Jade;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11C.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+    private int width, height;
+    private String title;
+    private long glfwWindow;
+
+    private static Window window = null;
+
+    private Window() {
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "Teste";
+    }
+
+    public static Window get() {
+        if (Window.window == null) {
+            Window.window = new Window();
+        }
+
+        return Window.window;
+    }
+
+    public void run() {
+        System.out.println("Hello LWJGL" + Version.getVersion());
+
+        init();
+        loop();
+
+        // Free the memory
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        // Terminate GLFW and error callack
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+
+    }
+
+    private void init() {
+        // Setup error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize
+        if (!glfwInit()){
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        // Config
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        // Create Window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create the GLFW window");
+        }
+
+        // OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+
+        // v-sync
+        glfwSwapInterval(1);
+
+        // visible window
+        glfwShowWindow(glfwWindow);
+
+        // detects the GLCapabilities instances and makes the OpenGL bindings available for use
+        GL.createCapabilities();
+    }
+
+    private void loop() {
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            // poll events
+            glfwPollEvents();
+
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(glfwWindow);
+
+        }
+    }
+}
